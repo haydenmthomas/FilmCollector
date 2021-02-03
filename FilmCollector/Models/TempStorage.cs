bd3d35b4538908d59354d1ef55224def9dbe226c
haydenmthomas <60405675+haydenmthomas@users.noreply.github.com>
2021-02-02 17:51:08 -0700

    First committ of Film Collector.

---
+++ FilmCollector/Models/TempStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmCollector.Models
+{
+    public static class TempStorage
+    {
+        private static List<FilmSubmission> films = new List<FilmSubmission>();
+
+        public static IEnumerable<FilmSubmission> Films => films;
+
+        public static void AddFilm(FilmSubmission film)
+        {
+            if (film.Title.ToLower() == "independence day")
+            {
+
+            }
+            else
+            {
+                films.Add(film);
+            }
+        }
+    }
+}
